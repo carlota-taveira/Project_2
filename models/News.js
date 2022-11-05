332182e0244677ca8207f58567fe3cc044342840
MarioBaltazar19 <peki-mario@hotmail.com>
2022-11-05 18:28:52 +0000

    mario 1 commit

---
+++ models/News.js
@@ -0,0 +1,18 @@
+const {Schema, model} = require('mongoose');
+
+const newsSchema = new Schema({
+    title: String, 
+    description: String,
+    pubDate: Number,
+    category: String,
+    country: String,
+    cast: [{type: Schema.Types.ObjectId, ref:"User"}]
+
+}, 
+{
+    timestamps: true,
+})
+
+const Article = model("User", newsSchema)
+
+module.exports = Article;
